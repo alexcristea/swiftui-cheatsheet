a775f1a04bcf5bfc046efeda6b5ad76a81783266
Alex Cristea <alexcristea@me.com>
2021-02-12 17:01:00 +0200

    Add text field formatter

--- Shared/Views/Texts/TextFieldTopicView.swift
+++ Shared/Views/Texts/TextFieldTopicView.swift
@@ -4,6 +4,9 @@ struct TextFieldTopicView: View {
     @State var username = ""
     @State var isEditing = false
 
+    @State private var nameComponents = PersonNameComponents()
+    let nameFormatter = PersonNameComponentsFormatter()
+
     static func make() -> AnyView {
         AnyView(erasing: self.init())
     }
@@ -39,6 +42,18 @@ struct TextFieldTopicView: View {
                 .background(isEditing ? Color.yellow.opacity(0.25) : Color.clear)
 
                 Text(username)
+                    .foregroundColor(.secondary)
+                    .font(.footnote)
+
+            }
+            VStack {
+                Text("Formatted text field")
+                    .font(.headline)
+                TextField("Enter your name", value: $nameComponents, formatter: nameFormatter)
+                    .padding(.all, 10)
+                    .border(Color.secondary)
+
+                Text(nameComponents.debugDescription)
                     .foregroundColor(.secondary)
                     .font(.footnote)
 
