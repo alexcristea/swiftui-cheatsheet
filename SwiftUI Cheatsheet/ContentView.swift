70bc63d5cee0df322fd03a665a80485dd82a06ba
Alex Cristea <alexcristea@me.com>
2021-02-12 12:27:55 +0200

    Initial Commit

---
+++ SwiftUI Cheatsheet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Cheatsheet
+//
+//  Created by Alex Cristea on 12.02.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI Cheatsheet/SwiftUI_CheatsheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CheatsheetApp.swift
+//  SwiftUI Cheatsheet
+//
+//  Created by Alex Cristea on 12.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CheatsheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
