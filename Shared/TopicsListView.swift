e9bce9264b4c10b186bdaf203f179375218b774e
Alex Cristea <alexcristea@me.com>
2021-02-12 15:11:18 +0200

    Set different list types based on content

--- Shared/TopicsListView.swift
+++ Shared/TopicsListView.swift
@@ -27,36 +27,57 @@ struct TopicsListView: View {
                 Topic(title: "Secure Field", icon: "rectangle.and.pencil.and.ellipsis"),
             ]),
         Topic(title: "Images", icon: "photo"),
-        Topic(title: "Buttons", icon: "plus.app.fill", children: [
-            Topic(title: "Button", icon: "plus.app"),
-            Topic(title: "Edit Button", icon: "square.and.pencil"),
-            Topic(title: "Paste Button", icon: "doc.on.clipboard.fill")
-        ]),
-        Topic(title: "Controls", icon: "gamecontroller.fill", children: [
-            Topic(title: "Menu", icon: "filemenu.and.selection"),
-            Topic(title: "Link", icon: "link"),
-            Topic(title: "Navigation Link", icon: "link.icloud.fill"),
-        ]),
-        Topic(title: "Value Selectors", icon: "hand.point.up.braille.fill", children: [
-            Topic(title: "Toggle", icon: "switch.2"),
-            Topic(title: "Picker", icon: "filemenu.and.cursorarrow"),
-            Topic(title: "Date Picker", icon: "calendar"),
-            Topic(title: "Slider", icon: "slider.horizontal.3"),
-            Topic(title: "Stepper", icon: "minus.slash.plus"),
-            Topic(title: "Color Picker", icon: "eyedropper.halffull"),
-        ]),
-        Topic(title: "Value Indicator", icon: "location", children: [
-            Topic(title: "Label", icon: "tag.fill"),
-            Topic(title: "Progress View", icon: "percent"),
-            Topic(title: "Gauge", icon: "gauge"),
-        ]),
+        Topic(
+            title: "Buttons", icon: "plus.app.fill",
+            children: [
+                Topic(title: "Button", icon: "plus.app"),
+                Topic(title: "Edit Button", icon: "square.and.pencil"),
+                Topic(title: "Paste Button", icon: "doc.on.clipboard.fill"),
+            ]),
+        Topic(
+            title: "Controls", icon: "gamecontroller.fill",
+            children: [
+                Topic(title: "Menu", icon: "filemenu.and.selection"),
+                Topic(title: "Link", icon: "link"),
+                Topic(title: "Navigation Link", icon: "link.icloud.fill"),
+            ]),
+        Topic(
+            title: "Value Selectors", icon: "hand.point.up.braille.fill",
+            children: [
+                Topic(title: "Toggle", icon: "switch.2"),
+                Topic(title: "Picker", icon: "filemenu.and.cursorarrow"),
+                Topic(title: "Date Picker", icon: "calendar"),
+                Topic(title: "Slider", icon: "slider.horizontal.3"),
+                Topic(title: "Stepper", icon: "minus.slash.plus"),
+                Topic(title: "Color Picker", icon: "eyedropper.halffull"),
+            ]),
+        Topic(
+            title: "Value Indicator", icon: "location",
+            children: [
+                Topic(title: "Label", icon: "tag.fill"),
+                Topic(title: "Progress View", icon: "percent"),
+                Topic(title: "Gauge", icon: "gauge"),
+            ]),
     ]
 
     var body: some View {
-        List(topics, children: \.children) { topic in
-            Label(topic.title, systemImage: topic.icon)
+        NavigationView {
+            #if os(macOS)
+                List(topics, children: \.children) { topic in
+                    Label(topic.title, systemImage: topic.icon)
+                }
+                .listStyle(SidebarListStyle())
+                .navigationTitle("Cheatsheet")
+                
+            #else
+                List(topics, children: \.children) { topic in
+                    Label(topic.title, systemImage: topic.icon)
+                }
+                .listStyle(InsetGroupedListStyle())
+                .navigationTitle("Cheatsheet")
+            #endif
+
         }
-        .listStyle(SidebarListStyle())
     }
 }
 
